e6c0771197584db628fd7eaa376f0948f94d23bc
mie <gawow11@gmail.com>
2023-03-21 13:49:17 +0900

    :tulip: add hover file

--- dist/js/script.js
+++ dist/js/script.js
@@ -212,51 +212,22 @@ IDEA 09
 ---------- */
 
 
-var mouseTargets = document.querySelectorAll('.js-mouse-item'); // if(mouseTargets !== null) {
-//     //ポインターの位置、座標
-//     let pointerX = 0;
-//     let pointerY = 0;
-//     let x = 0;
-//     let y = 0;
-//     //最小値、最大値
-//     const minmax = (num) => {
-//         return Math.min( 0.5, Math.max(-0.5,num)); //-0.5以上0.5以下
-//     }
-//     const coordinate = () => {
-//         //表示場所の位置
-//         mouseTargets.forEach(target => {
-//             const wrapReact = target.getBoundingClientRect();
-//             // console.log(wrapReact);
-//             //ポインターが表示場所のどの位置にあるか。中心を(0,0)とする為に0.5引く
-//             x = (pointerX - wrapReact.left) / wrapReact.width - 0.5;
-//             y = (pointerY - wrapReact.top) / wrapReact.height - 0.5;
-//             //最小値、最大値の確認（touchイベント用）
-//             x = minmax(x);
-//             y = minmax(y);
-//         })
-//     }
-//     mouseTargets.forEach(target => {
-//         target.addEventListener('mousemove', e => {
-//             pointerX = e.clientX;
-//             pointerY = e.clientY;
-//             coordinate();
-//             target.style.transform = `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg) scale3d(1, 1, 1)`;
-//         })
-//     })
-// }
+var mouseTargets = document.querySelectorAll('.js-mouse-item');
 
 if (mouseTargets !== null) {
   mouseTargets.forEach(function (target) {
     target.addEventListener('mousemove', function (e) {
-      var pointerX = target.clientWidth - e.clientX * 2;
-      var pointerY = target.clientHeight - e.clientY * 2;
+      var pointerX = target.clientWidth - e.offsetX * 2;
+      var pointerY = target.clientHeight - e.offsetY * 2;
       var x = -pointerX / 30;
-      var y = pointerY / 24;
-      target.style.transform = "perspective(1000px) rotateX(".concat(x, "deg) rotateY(").concat(y, "deg) scale3d(1, 1, 1)");
+      var y = pointerY / 25;
+      target.style.transform = "perspective(1000px) rotateX(".concat(x, "deg) rotateY(").concat(y, "deg) scale(1.05)");
+      console.log("rotateX(".concat(x, "deg)"));
+      console.log("rotateY(".concat(y, "deg)"));
     }, false);
     target.addEventListener('mouseleave', function () {
       mouseTargets.forEach(function (target) {
-        target.style.transform = "perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)";
+        target.style.transform = "perspective(1000px) rotateX(0deg) rotateY(0deg) scale(1.05)";
       });
     });
   });
